80bd03fc6ed5875322899b53fd3ca73695bf9f7d
Allen Chen <allchen@expedia.com>
2017-11-30 12:24:31 -0800

    player dom created

---
+++ controls.js
@@ -0,0 +1,16 @@
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubePlayerAPIReady() {
+  player = new YT.Player('ytplayer', {
+    height: '360',
+    width: '640',
+    playerVars: {
+      listType: 'playlist',
+      list:'PL3C6509FB6DE254C6'
+    }
+  });
+}
